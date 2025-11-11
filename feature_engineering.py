687fa0ca2109eb38bbb99c251e14b07fe428398c
Atla Vijaya Kumari <atlavijayakumari@gmail.com>
2025-11-11 20:23:15 +0530

    Correct data processing logic in feature_engineering.py
    Fixed syntax errors in data processing and aggregation.

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,23 +1,39 @@
 import pandas as pd
-data = pd.read_csv("sample_transactions.csv")
-print("Dataset loaded successfully:\n", data.head(
-total_spent = data.groupby("CustomerID")["PurchaseAmount"].sum().reset_index()
-purchase_freq = (
-    data.groupby("CustomerID")["ProductID"]
-    .count()
-    .reset_index()
-    .rename(columns={"ProductID": "TotalPurchases"})
-)
-    for each customer
-top_category = (
-    data.groupby("CustomerID")["Category"]
-    .agg(lambda x: x.mode()[0])
-    .reset_index()
-    .rename(columns={"Category": "FavoriteCategory"})
-)
-customer_summary = (
-    total_spent.merge(purchase_freq, on="CustomerID")
-    .merge(top_category, on="CustomerID")
-)
-print("\nCustomer Summary:\n", customer_summary.head())
 
+data = pd.read_csv("sample_transactions.csv")
+
+print("Dataset loaded successfully:\n", data.head(
+
+total_spent = data.groupby("CustomerID")["PurchaseAmount"].sum().reset_index()
+
+purchase_freq = (
+
+    data.groupby("CustomerID")["ProductID"]
+
+    .count
+
+    .reset_index
+
+    .rename(columns={"ProductID": "TotalPurchases"})
+
+)
+
+    for each customer
+
+top_category = (
+
+    data.groupby("CustomerID")["Category"]
+
+    .agg(lambda x: x.mode()[0])
+
+    .reset_index
+
+    .rename(columns={"Category": "FavoriteCategory)
+
+    total_spent.merge(purchase_freq, on="CustomerID")
+
+    .merge(top_category, on="CustomerID")
+
+)
+
+print("\nCustomer Summary:\n", customer_summary.head())
